3802a1e5b712047af7f6193c8483c263ad27034f
Alexey Lashnev <lashnag@gmail.com>
2026-02-15 19:53:06 +0300

    Log fix v4

--- app/logger.py
+++ app/logger.py
@@ -9,7 +9,13 @@ from logstash_async.handler import AsynchronousLogstashHandler
 request_headers = contextvars.ContextVar('request_headers')
 
 def is_remote_logger():
-    return os.getenv('REMOTE_LOGGER') is not None
+    env_value = os.getenv('REMOTE_LOGGER', '').lower()
+    if env_value == 'true':
+        logging.getLogger().info("Remote logger")
+        return True
+    else:
+        logging.getLogger().info("Local logger")
+        return False
 
 def init_logger():
     handler = AsynchronousLogstashHandler(
@@ -18,22 +24,13 @@ def init_logger():
         database_path=None,
     ) if is_remote_logger() else logging.StreamHandler(sys.stdout)
     handler.setFormatter(JsonFormatter())
-    logging_level = logging.INFO if is_remote_logger() else logging.DEBUG
-
-    root_logger = logging.getLogger()
-    root_logger.setLevel(logging_level)
-    root_logger.handlers = []
-    root_logger.addHandler(handler)
-
     logging.basicConfig(
-        level=logging_level,
+        level=logging.INFO if is_remote_logger() else logging.DEBUG,
         datefmt = "%Y-%m-%d %H:%M:%S",
         handlers = [handler]
     )
 
-    mode = "Remote" if is_remote_logger() else "Local"
-    logging.getLogger().info(f"{mode} logger initialized. Prod mode: {is_remote_logger()}")
-    print(f"{mode} logger initialized. Prod mode: {is_remote_logger()}. Using print")
+    logging.getLogger().info(f"Prod mode: {is_remote_logger()}")
 
 class JsonFormatter(logging.Formatter):
     def format(self, record):
